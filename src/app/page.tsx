af69100d181b1bb759337d86f12f7487725bb8bf
Md Abu Bokar <mdabu0212@gmail.com>
2025-09-13 04:29:46 +0600

    initial commit and component setup

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Navbar from "@/components/sections/Navbar";
+import Hero from "@/components/sections/Hero";
+import About from "@/components/sections/About";
+import Why from "@/components/sections/Why";
+import How from "@/components/sections/How";
+import Join from "@/components/sections/Join";
+import Footer from "@/components/sections/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Why />
+      <How />
+      <Join />
+      <Footer />
+    </div>
+  );
+}
